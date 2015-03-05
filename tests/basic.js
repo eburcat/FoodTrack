66bf7fa841c82f82e3e0bcc1ce60c14e74fd5654
Eitan Burcat <eitanb@r-net.co.il>
2015-03-06 00:20:23 +0200

    tests: toEqual is more appropriate than toBe with numbers

--- tests/basic.js
+++ tests/basic.js
@@ -19,19 +19,19 @@ describe("FoodTrack module", function () {
 
         it("should start with 0ed stuff", function () {
             controller("dayController", {$scope: scope});
-            expect(scope.today["Grains"]).toBe(0);
-            expect(scope.today["Fats"]).toBe(0);
+            expect(scope.today["Grains"]).toEqual(0);
+            expect(scope.today["Fats"]).toEqual(0);
         });
 
         it("should be able to increase and decrease, but not below 0", function () {
             controller("dayController", {$scope: scope});
-            expect(scope.today["Grains"]).toBe(0);
+            expect(scope.today["Grains"]).toEqual(0);
             scope.add("Grains", 1);
-            expect(scope.today["Grains"]).toBe(1);
+            expect(scope.today["Grains"]).toEqual(1);
             scope.add("Grains", -1);
-            expect(scope.today["Grains"]).toBe(0);
+            expect(scope.today["Grains"]).toEqual(0);
             scope.add("Grains", -1);
-            expect(scope.today["Grains"]).toBe(0);
+            expect(scope.today["Grains"]).toEqual(0);
         });
     });
 });
